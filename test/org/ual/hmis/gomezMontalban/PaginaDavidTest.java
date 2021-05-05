10249eaededd18f4db010217ed4c1551266a87e6
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-05-05 21:42:16 +0200

    drivers comentados

--- test/org/ual/hmis/gomezMontalban/PaginaDavidTest.java
+++ test/org/ual/hmis/gomezMontalban/PaginaDavidTest.java
@@ -33,8 +33,8 @@ public class PaginaDavidTest {
 
 	@Before
 	public void setUp() {
-		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		FirefoxOptions firefoxOptions = new FirefoxOptions();
 		firefoxOptions.setHeadless(true);
 		driver = new FirefoxDriver(firefoxOptions);
